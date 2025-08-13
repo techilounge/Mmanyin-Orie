5b1f3b5a92939a0eb850605fec1c2e7b12384671
TechiLounge <techilounge@gmail.com>
2025-08-13 10:57:16 +0000

    I rotate the API key earlier but the one in /src/lib/firebase.ts is stil

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  "projectId": "mmanyin-orie",
-  "appId": "1:682483710818:web:2d83085f55f3aba238ca3b",
-  "storageBucket": "mmanyin-orie.firebasestorage.app",
-  "apiKey": "AIzaSyBUbeZbtpCZczcoV5c6dzC-uruSvZ3myns",
-  "authDomain": "mmanyin-orie.firebaseapp.com",
-  "messagingSenderId": "682483710818"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
